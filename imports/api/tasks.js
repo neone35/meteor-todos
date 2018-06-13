c5495fdb779d748d80da277494d268c25d601635
Artur Maslov <aartasas@gmail.com>
2018-06-13 22:40:55 +0300

    Add authUI with security & validation check methods in DB

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,5 +1,82 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 // On server sets up a MongoDB collection 
 // On client creates a cache connected to the server collection
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+    // This code only runs on the server
+    Meteor.publish('tasks', function tasksPublication() { // subscribe on client
+        // user is logged in
+        if (this.userId) {
+            return Tasks.find({ owner: this.userId });
+        } else {
+            return Tasks.find({
+                $or: [
+                    { private: { $ne: true } },
+                    { owner: this.userId },
+                ],
+            });
+        }
+    });
+}
+
+Meteor.methods({
+    'tasks.insert'(text) {
+        check(text, String);
+
+        // Make sure the user is logged in before inserting a task
+        if (!this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+            owner: this.userId,
+            username: Meteor.users.findOne(this.userId).username,
+        });
+    },
+    'tasks.remove'(taskId) {
+        check(taskId, String);
+
+        const task = Tasks.findOne(taskId);
+        // only owner can remove private task
+        if (task.private && task.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+        // only owner can remove public task
+        if (task.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.remove(taskId);
+    },
+    'tasks.setChecked'(taskId, setChecked) {
+        check(taskId, String);
+        check(setChecked, Boolean);
+
+        const task = Tasks.findOne(taskId);
+        // only owner can make private task public task
+        if (task.private && task.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.update(taskId, { $set: { checked: setChecked } });
+    },
+    'tasks.setPrivate'(taskId, setToPrivate) {
+        check(taskId, String);
+        check(setToPrivate, Boolean);
+
+        const task = Tasks.findOne(taskId);
+
+        // Make sure only the task owner can make a task private
+        if (task.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.update(taskId, { $set: { private: setToPrivate } });
+    },
+});
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,44 +1,85 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 // allows us to create a "data container" to feed Meteor's reactive data into React's component hierarchy.
 import { withTracker } from 'meteor/react-meteor-data';
 // creates a cache connected to the server collection
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
-            <Task key={task._id} task={task} />
-        ));
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => {
+            // if first is false, result is first
+            // if first is true, result is second
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButtons = task.owner === currentUserId;
+
+            return (
+                <Task
+                    key={task._id}
+                    task={task}
+                    showPrivateButtons={showPrivateButtons}
+                />
+            );
+        });
     }
 
     handleSubmit(event) {
         event.preventDefault();
         // Find the text field via the React ref
         const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-        Tasks.insert({
-            text,
-            createdAt: new Date(),
-        });
+        Meteor.call('tasks.insert', text);
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
     }
 
     render() {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
 
-                    <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                    <label className="hide-completed">
                         <input
-                            type="text"
-                            ref="textInput"
-                            placeholder="Type to add new tasks"
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
                         />
-                    </form>
+                        Hide Completed Tasks
+                    </label>
+
+                    <AccountsUIWrapper />
+
+                    {this.props.currentUser ?
+                        <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                            <input
+                                type="text"
+                                ref="textInput"
+                                placeholder="Type to add new tasks"
+                            />
+                        </form> : ''
+                    }
                 </header>
 
                 <ul>
@@ -52,9 +93,12 @@ class App extends Component {
 // fetches tasks from the Tasks collection
 // in a reactive way, so that when the contents of the database change, the App re-renders
 export default withTracker(() => {
+    Meteor.subscribe('tasks');
     return {
         tasks: Tasks.find({}, 
             { sort: { createdAt: 'descending' } })
             .fetch(),
+        incompleteCount: Tasks.find({ checked: false }).count(),
+        currentUser: Meteor.user(),
     };
 })(App);
